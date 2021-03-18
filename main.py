d8f58dcf76d19233919d82622857ebbec05144df
mickey1390 <michal.kryjan@gmail.com>
2021-03-18 17:29:16 +0100

    added UI for adding a new task and saving it to database, few improvements in Task class and database tasks

--- main.py
+++ main.py
@@ -1,55 +1,72 @@
 import sqlite3
 import os.path
-from datetime import date, time
+from datetime import date, datetime
+
+
+def createConnection():
+    conn = None
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DB_PATH = os.path.join(BASE_DIR, "scheduler.db")
+    try:
+        conn = sqlite3.connect(DB_PATH)
+    except sqlite3.Error as e:
+        print(e)
+    return conn
 
 
 class Task:
-    def __init__(self, name, description, deadline):
+    def __init__(self, name, description, deadline, is_urgent):
         self.name = name
         self.description = description
         self.deadline = deadline
-        self.date_added = date.today()
-        self.time_added = time.now()
+        self.time_added = datetime.now()
+        self.is_urgent = is_urgent
         self.status = 'new'
 
-    def add_task_to_db(self):
+    def addTaskToDb(self):
         try:
-            sqliteConnection = sqlite3.connect(DB_PATH)
+            sqliteConnection = createConnection()
             db = sqliteConnection.cursor()
-            sqlite_add_task_query = f'''INSERT INTO tasks VALUES 
-                                        ("{self.name}", 
-                                        "{self.description}",
-                                        "{self.deadline}", 
-                                        "{self.date_added}", 
-                                        "{self.time_added}", 
-                                        "{self.status}")'''
-            db.execute(sqlite_add_task_query)
-            db.commit()
+            db.execute("INSERT INTO tasks(name, description, deadline, is_urgent, time_added, status) VALUES (?, ?, ?, ?, ?, ?)", (str(self.name), str(self.description), str(self.deadline), str(self.is_urgent), str(self.time_added), str(self.status)))
+            sqliteConnection.commit()
+            print('successfully added')
         except sqlite3.Error as error:
             print("Error while adding a new task to db", error)
         finally:
             if sqliteConnection:
                 sqliteConnection.close()
 
+def startDb():
+    sqliteConnection = createConnection()
+    try:
+        db = sqliteConnection.cursor()
+        create_table_query = '''CREATE TABLE IF NOT EXISTS tasks (
+                                        id INTEGER PRIMARY KEY,
+                                        name TEXT NOT NULL,
+                                        description TEXT,
+                                        deadline DATETIME NOT NULL,
+                                        is_urgent TEXT NOT NULL,
+                                        time_added DATETIME NOT NULL,
+                                        status TEXT NOT NULL);'''
+        db.execute(create_table_query)
+    except sqlite3.Error as error:
+        print("Error while creating a new table", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "scheduler.db")
-
-try:
-    sqliteConnection = sqlite3.connect(DB_PATH)
-    db = sqliteConnection.cursor()
-    sqlite_create_table_query = '''CREATE TABLE IF NOT EXISTS tasks (
-                                    id INTEGER PRIMARY KEY,
-                                    name TEXT NOT NULL,
-                                    description TEXT,
-                                    deadline TEXT,
-                                    date_added_text TEXT NOT NULL,
-                                    time_added_text TEXT NOT NULL,
-                                    status TEXT NOT NULL);'''
-    db.execute(sqlite_create_table_query)
-
-except sqlite3.Error as error:
-    print("Error while creating a new table", error)
-finally:
-    if sqliteConnection:
-        sqliteConnection.close()
+def getAllTasks():
+    sqliteConnection = createConnection()
+    try:
+        db = sqliteConnection.cursor()
+        db.execute("SELECT * FROM tasks")
+        rows = db.fetchall()
+        if len(rows) == 0:
+            print('No tasks')
+        for row in rows:
+            print(row)
+    except sqlite3.Error as error:
+        print("Error while selecting all tasks", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
